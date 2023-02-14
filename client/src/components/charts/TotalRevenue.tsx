f48a60bdc2da97d7886545a57fd11eb973c9e12a
FelipeMDantas <felipemdantas@terra.com.br>
2023-02-13 23:39:33 -0300

    Total Revenue and Property Referrals charts added via ReactApexChart.

--- client/src/components/charts/TotalRevenue.tsx
+++ client/src/components/charts/TotalRevenue.tsx
@@ -1,5 +1,7 @@
 import { ArrowCircleUpRounded } from "@mui/icons-material";
 import { Box, Stack, Typography } from "@pankod/refine-mui";
+import ReactApexChart from "react-apexcharts";
+import { TotalRevenueOptions, TotalRevenueSeries } from "./chart.config";
 
 const TotalRevenue = () => {
   return (
@@ -16,11 +18,11 @@ const TotalRevenue = () => {
         Total Revenue
       </Typography>
 
-      <Stack my={20} direction="row" gap={4} flexWrap="wrap">
-        <Typography fontSize={28} fontWeight={700} color="#11142d">
-          $236, 535
+      <Stack my="20px" direction="row" gap={4} flexWrap="wrap">
+        <Typography fontSize={28} fontWeight={500} color="#11142d">
+          $236,535
         </Typography>
-        <Stack>
+        <Stack direction="row" alignItems="center" gap={1}>
           <ArrowCircleUpRounded sx={{ fontSize: 25, color: "#475be8" }} />
           <Stack>
             <Typography fontSize={15} color="#475be8">
@@ -32,6 +34,13 @@ const TotalRevenue = () => {
           </Stack>
         </Stack>
       </Stack>
+
+      <ReactApexChart
+        series={TotalRevenueSeries}
+        type="bar"
+        height={310}
+        options={TotalRevenueOptions}
+      />
     </Box>
   );
 };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
+import "index.css";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
