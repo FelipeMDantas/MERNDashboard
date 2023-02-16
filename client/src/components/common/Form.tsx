69332e85e06efa794d3e7120fd03a4978a6f4dfb
FelipeMDantas <felipemdantas@terra.com.br>
2023-02-15 23:47:04 -0300

    Elements added to Form component.

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -1,4 +1,11 @@
-import { Box, Typography } from "@pankod/refine-mui";
+import {
+  Box,
+  FormControl,
+  FormHelperText,
+  TextareaAutosize,
+  TextField,
+  Typography,
+} from "@pankod/refine-mui";
 import { FormProps } from "interfaces/common";
 
 const Form = ({
@@ -15,6 +22,66 @@ const Form = ({
       <Typography fontSize={25} fontWeight={700} color="#11142d">
         {type} a property
       </Typography>
+
+      <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="#fcfcfc">
+        <form
+          style={{
+            marginTop: "20px",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+          }}
+          onSubmit={handleSubmit(onFinishHandler)}
+        >
+          <FormControl>
+            <FormHelperText
+              sx={{
+                fontWeight: 500,
+                margin: "10px",
+                fontSize: 16,
+                color: "#11142d",
+              }}
+            >
+              Enter property name
+            </FormHelperText>
+            <TextField
+              fullWidth
+              required
+              id="outlined-basic"
+              color="info"
+              variant="outlined"
+              {...register("title", { required: true })}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormHelperText
+              sx={{
+                fontWeight: 500,
+                margin: "10px",
+                fontSize: 16,
+                color: "#11142d",
+              }}
+            >
+              Enter description
+            </FormHelperText>
+            <TextareaAutosize
+              minRows={5}
+              required
+              placeholder="Write description"
+              color="info"
+              style={{
+                width: "100%",
+                background: "transparent",
+                fontSize: "16px",
+                borderColor: "rgba(0,0,0,0.23)",
+                borderRadius: 6,
+              }}
+            />
+          </FormControl>
+        </form>
+      </Box>
     </Box>
   );
 };
