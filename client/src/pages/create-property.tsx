ff9564c6d50b2943a4a28377200dc1783dafcba1
FelipeMDantas <felipemdantas@terra.com.br>
2023-02-18 22:43:40 -0300

    Server configured and connection to MongoDB established.

--- client/src/pages/create-property.tsx
+++ client/src/pages/create-property.tsx
@@ -1,5 +1,5 @@
 import { useGetIdentity } from "@pankod/refine-core";
-import { useForm } from "@pankod/refine-react-hook-form";
+import { FieldValues, useForm } from "@pankod/refine-react-hook-form";
 import { useNavigate } from "@pankod/refine-react-router-v6";
 import Form from "components/common/Form";
 import { useState } from "react";
@@ -14,8 +14,24 @@ const CreateProperty = () => {
     handleSubmit,
   } = useForm();
 
-  const handleImageChange = () => {};
-  const onFinishHandler = () => {};
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) =>
+      new Promise<string>((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.onload = () => resolve(fileReader.result as string);
+        fileReader.readAsDataURL(readFile);
+      });
+
+    reader(file).then((result: string) =>
+      setPropertyImage({ name: file?.name, url: result })
+    );
+  };
+
+  const onFinishHandler = async (data: FieldValues) => {
+    if (!propertyImage.name) return alert("Please select an image.");
+
+    await onFinish({ ...data, photo: propertyImage.url, email: user.email });
+  };
 
   return (
     <Form
