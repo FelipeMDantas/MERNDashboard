5f5fda9cc172d4f455395d4150101a223abeea2e
FelipeMDantas <felipemdantas@terra.com.br>
2023-02-21 21:12:24 -0300

    Data registration and retrieval established.

--- client/src/components/common/PropertyCard.tsx
+++ client/src/components/common/PropertyCard.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { Card, CardMedia } from "@pankod/refine-mui";
+import { Link } from "@pankod/refine-react-router-v6";
+import { PropertyCardProps } from "interfaces/property";
 
-const PropertyCard = () => {
-  return <div>PropertyCard</div>;
+const PropertyCard = ({
+  id,
+  title,
+  location,
+  price,
+  photo,
+}: PropertyCardProps) => {
+  return (
+    <Card
+      component={Link}
+      to={`/properties/${id}`}
+      sx={{
+        maxWidth: "330px",
+        padding: "10px",
+        "&:hover": {
+          boxShadow: "0 22px 45px 2px rgba(176, 176, 176, 0.1)",
+        },
+        cursor: "pointer",
+      }}
+      elevation={0}
+    >
+      <CardMedia
+        component="img"
+        width="100%"
+        height={210}
+        image={photo}
+        alt="card image"
+        sx={{ borderRadius: "10px" }}
+      />
+    </Card>
+  );
 };
 
 export default PropertyCard;
--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,10 +1,20 @@
 import { Add } from "@mui/icons-material";
+import { useTable } from "@pankod/refine-core";
 import { Box, Stack, Typography } from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
-import { CustomButton } from "components";
+import { CustomButton, PropertyCard } from "components";
 
 const AllProperties = () => {
   const navigate = useNavigate();
+
+  const {
+    tableQueryResult: { data, isLoading, isError },
+  } = useTable();
+
+  const allProperties = data?.data ?? [];
+
+  if (isLoading) return <Typography>Loading...</Typography>;
+  if (isError) return <Typography>Error...</Typography>;
 
   return (
     <Box>
@@ -20,6 +30,19 @@ const AllProperties = () => {
           handleClick={() => navigate("/properties/create")}
         />
       </Stack>
+
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        {allProperties.map((property) => (
+          <PropertyCard
+            key={property._id}
+            id={property._id}
+            title={property.title}
+            price={property.price}
+            location={property.location}
+            photo={property.photo}
+          />
+        ))}
+      </Box>
     </Box>
   );
 };
