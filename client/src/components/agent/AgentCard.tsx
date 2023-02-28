2905d1f1e6a02ef889461c3c29b5faeb120087de
FelipeMDantas <felipemdantas@terra.com.br>
2023-02-27 23:57:02 -0300

    Agent page development started.

--- client/src/components/agent/AgentCard.tsx
+++ client/src/components/agent/AgentCard.tsx
@@ -1,7 +1,75 @@
-import React from "react";
+import { useGetIdentity } from "@pankod/refine-core";
+import { Box, Stack, Typography } from "@pankod/refine-mui";
+import { Link } from "@pankod/refine-react-router-v6";
+import { AgentCardProp, InfoBarProps } from "interfaces/agent";
 
-const AgentCard = () => {
-  return <div>AgentCard</div>;
+const InfoBar = ({ icon, name }: InfoBarProps) => (
+  <Stack>
+    {icon}
+    <Typography>{name}</Typography>
+  </Stack>
+);
+
+const AgentCard = ({
+  id,
+  name,
+  email,
+  avatar,
+  noOfProperties,
+}: AgentCardProp) => {
+  const { data: currentUser } = useGetIdentity();
+
+  const generateLink = () => {
+    if (currentUser.email === email) return "/my-profile";
+
+    return `/agent/show/${id}`;
+  };
+  return (
+    <Box
+      component={Link}
+      to={generateLink()}
+      width="100%"
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", sm: "row" },
+        gap: "20px",
+        padding: "20px",
+        "&:hover": {
+          boxShadow: "0 22px 45px 2px rgba(176,176,176,0.1)",
+        },
+      }}
+    >
+      <img
+        src={avatar}
+        alt="user"
+        width={90}
+        height={90}
+        style={{ borderRadius: 8, objectFit: "cover" }}
+      />
+      <Stack
+        direction="column"
+        justifyContent="space-between"
+        flex={1}
+        gap={{ xs: 4, sm: 2 }}
+      >
+        <Stack gap={2} direction="row" flexWrap="wrap" alignItems="center">
+          <Typography fontSize={22} fontWeight={600} color="#11142d">
+            {name}
+          </Typography>
+          <Typography fontSize={14} color="#808191">
+            Real-Estate Agent
+          </Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          flexWrap="wrap"
+          justifyContent="space-between"
+          alignItems="center"
+          gap={2}
+        ></Stack>
+      </Stack>
+    </Box>
+  );
 };
 
 export default AgentCard;
--- client/src/pages/agent.tsx
+++ client/src/pages/agent.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { useList } from "@pankod/refine-core";
+import { Box, Typography } from "@pankod/refine-mui";
+import { AgentCard } from "components";
 
-const agent = () => {
-  return <div>agent</div>;
+const Agent = () => {
+  const { data, isLoading, isError } = useList({
+    resource: "users",
+  });
+
+  const allAgents = data?.data ?? [];
+
+  if (isLoading) return <div>loading...</div>;
+  if (isError) return <div>error...</div>;
+
+  return (
+    <Box>
+      <Typography fontWeight={700} fontSize={25} color="#11142d">
+        Agents List
+      </Typography>
+
+      <Box
+        mt="20px"
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "20px",
+          backgroundColor: "#fcfcfc",
+        }}
+      >
+        {allAgents.map((agent) => (
+          <AgentCard
+            key={agent._id}
+            id={agent._id}
+            name={agent.name}
+            email={agent.email}
+            avatar={agent.avatar}
+            noOfProperties={agent.allProperties.length}
+          />
+        ))}
+      </Box>
+    </Box>
+  );
 };
 
-export default agent;
+export default Agent;
