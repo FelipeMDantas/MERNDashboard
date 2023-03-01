72dcf123e4763db9ad13f64d77371deb444eb362
FelipeMDantas <felipemdantas@terra.com.br>
2023-02-28 23:55:21 -0300

    InfoBar component stylized and applied.

--- client/src/components/agent/AgentCard.tsx
+++ client/src/components/agent/AgentCard.tsx
@@ -1,12 +1,15 @@
+import { EmailOutlined, LocationCity, Phone, Place } from "@mui/icons-material";
 import { useGetIdentity } from "@pankod/refine-core";
 import { Box, Stack, Typography } from "@pankod/refine-mui";
 import { Link } from "@pankod/refine-react-router-v6";
 import { AgentCardProp, InfoBarProps } from "interfaces/agent";
 
 const InfoBar = ({ icon, name }: InfoBarProps) => (
-  <Stack>
+  <Stack flex={1} minWidth={{ xs: "100%", sm: 300 }} gap={1.5} direction="row">
     {icon}
-    <Typography>{name}</Typography>
+    <Typography fontSize={14} color="#8088191">
+      {name}
+    </Typography>
   </Stack>
 );
 
@@ -66,7 +69,21 @@ const AgentCard = ({
           justifyContent="space-between"
           alignItems="center"
           gap={2}
-        ></Stack>
+        >
+          <InfoBar
+            icon={<EmailOutlined sx={{ color: "#808191" }} />}
+            name={email}
+          />
+          <InfoBar icon={<Place sx={{ color: "#808191" }} />} name="London" />
+          <InfoBar
+            icon={<Phone sx={{ color: "#808191" }} />}
+            name="+502-3231-4141"
+          />
+          <InfoBar
+            icon={<LocationCity sx={{ color: "#808191" }} />}
+            name={`${noOfProperties} Properties`}
+          />
+        </Stack>
       </Stack>
     </Box>
   );
